66a84db4d1a33c39033011b50110c9ba0b731407
DyEsSuCr <2000sajo@gmail.com>
2023-12-06 14:31:43 -0500

    fix: router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,24 +1,10 @@
 import { Router } from 'express'
-import { readdirSync } from 'node:fs'
-import { parse, join } from 'node:path'
+import { router as routerDrivers } from '../modules/drivers/drivers.routes'
+import { router as routerVehicles } from '../modules/vehicles/vehicles.routes'
 
-const pathModules = join(__dirname, '../modules')
 const routes = Router()
 
-const cleanFileName = (fileName: string): string => parse(fileName).name
-
-readdirSync(pathModules).forEach((folder): void => {
-  const pathRoute: string = join(pathModules, folder)
-
-  readdirSync(pathRoute).forEach((fileName): void => {
-    const cleanName = cleanFileName(fileName)
-
-    if (cleanName.endsWith('routes')) {
-      import(`${pathRoute}/${cleanName}`).then((moduleRouter): void => {
-        routes.use(`/api/v1/${String(cleanName.split('.').shift())}`, moduleRouter.router)
-      })
-    }
-  })
-})
+routes.use('/api/v1/vehicles', routerVehicles)
+routes.use('/api/v1/drivers', routerDrivers)
 
 export { routes }
